646a0b3b526272009ee5d564bcf4e914b9274e52
Jonas Oscarsson <jonas.oscarsson@valtech.se>
2013-05-03 16:59:14 +0200

    Makes job list empty if Jenkins is unreachable

--- jobs/jenkins.rb
+++ jobs/jenkins.rb
@@ -5,36 +5,41 @@ port = 8081
 last_builds = {}
 
 SCHEDULER.every '10s', :first_in => 0 do |foo|
-  http = Net::HTTP.new(jenkins_host, port)
-  response = http.request(Net::HTTP::Get.new("/jenkins/view/Dashboard/api/json?depth=1"))
-  jobs = JSON.parse(response.body)["jobs"]
-
   builds = []
 
-  jobs.map! do |job|
-    name = job['name']
-    color = job['color'].sub('blue', 'green')
-    status = case color
-               when 'green' then 'Success'
-               when 'yellow' then 'Unstable'
-               when 'disabled' then 'Disabled'
-               when 'gray' then 'Disabled'
-               when 'aborted' then 'Aborted'
-               when 'green_anime' then 'Building'
-               when 'red_anime' then 'Building'
-               when 'gray_anime' then 'Building'
-               when 'aborted_anime' then 'Building'
-               when 'yellow_anime' then 'Building'
-               else 'Failure'
-             end
-    icon_url = job['healthReport'][0]['iconUrl']
-    health_url = "http://#{jenkins_host}:#{port}#{img_path}#{icon_url}"
-    desc = job['description']
-    build = {
-      name: name, status: status, health: health_url, color: color
-    }
-    desc.empty? || build['desc'] = desc
-    builds << build
+  begin
+    http = Net::HTTP.new(jenkins_host, port)
+    response = http.request(Net::HTTP::Get.new("/jenkins/view/Dashboard/api/json?depth=1"))
+
+    jobs = JSON.parse(response.body)["jobs"]
+
+    jobs.map! do |job|
+      name = job['name']
+      color = job['color'].sub('blue', 'green')
+      status = case color
+                 when 'green' then 'Success'
+                 when 'yellow' then 'Unstable'
+                 when 'disabled' then 'Disabled'
+                 when 'gray' then 'Disabled'
+                 when 'aborted' then 'Aborted'
+                 when 'green_anime' then 'Building'
+                 when 'red_anime' then 'Building'
+                 when 'gray_anime' then 'Building'
+                 when 'aborted_anime' then 'Building'
+                 when 'yellow_anime' then 'Building'
+                 else 'Failure'
+               end
+      icon_url = job['healthReport'][0]['iconUrl']
+      health_url = "http://#{jenkins_host}:#{port}#{img_path}#{icon_url}"
+      desc = job['description']
+      build = {
+        name: name, status: status, health: health_url, color: color
+      }
+      desc.empty? || build['desc'] = desc
+      builds << build
+    end
+  rescue Exception
+    
   end
 
   if builds != last_builds
